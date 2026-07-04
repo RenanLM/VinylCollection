c2d9521f1ab668294848833136d448bf374c2232
RenanLM <renanlm691@gmail.com>
2026-07-03 22:20:09 -0300

    build-fix: fixing dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,11 +6,7 @@ plugins {
 
 android {
     namespace = "br.com.renan.vinylcollection"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "br.com.renan.vinylcollection"
